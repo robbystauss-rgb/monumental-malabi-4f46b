344ec6f3e914146c4311dd34357ef4c4fb47e0bf
robbystauss-rgb <robbystauss@gmail.com>
2026-08-18 18:28:00 -0500

    Add verified linked hat color catalog for website builder

---
+++ assets/hat-catalog-client.js
@@ -0,0 +1,40 @@
+window.REC_HAT_CATALOG = {
+  "112": {
+    label: "112 Trucker",
+    priceTier: "standard",
+    colors: [
+      "White","Black","Cardinal","Columbia Blue","Biscuit/True Blue","Quarry","Red","Royal","Dark Green","Loden","Orange","Navy","Purple","Amber Gold","Charcoal","Coffee","Cream","Kelly","Light Blue","Light Grey","Maroon","Smoke Blue","Khaki/Coffee","Purple/White","Khaki/Legion Blue","Heather Grey/Dark Green","Charcoal/Black","Charcoal/Royal","Khaki/White","Khaki/Loden","Heather Grey/Light Grey","Charcoal/Columbia Blue","Charcoal/White","Loden/Black","Khaki/Navy","Heather Grey/Navy","Charcoal/Kelly","Chocolate Chip/Birch","Black/Charcoal","Black/Gold","Royal/Black","Maroon/White","White/Black","Heather Grey/Red","Columbia Blue/Black","Charcoal/Navy","Black/Vegas Gold","Royal/Red","White/Charcoal","Heather Grey/Royal","Columbia Blue/Khaki","Charcoal/Neon Blue","Black/White","Heather Grey/White","Navy/Khaki","Cyan/White","Black/Yellow","White/Kelly","Hot Pink/Black","Charcoal/Neon Orange","Navy/Orange","Brown/Khaki","White/Navy","Hot Pink/White","Charcoal/Neon Pink","Dark Green/Gold","Kelly/Black","White/Red","Caramel/Black","Charcoal/Neon Yellow","Navy/White","Dark Green/White","Kelly/White","Orange/Black","Khaki/Black","White/Royal","Cardinal/Black","Charcoal/Orange","Dark Green/Yellow","Khaki/Burgundy","Orange/White","Khaki/Chocolate Chip","Cardinal/White","Charcoal/Red","White/Columbia Blue","White/Dark Green","Navy/White/Red","Grey/Charcoal/Black","Orange/White/Black","Grey/Charcoal/Navy","Red/White/Black","Heather Grey/Birch/Amber Gold","Heather Grey/Birch/Army Olive","Heather Grey/Cardinal/Navy","Red/White/Navy","Royal/White/Heather Grey","Black/White/Heather Grey","Royal/White/Red","Heather Grey/Charcoal/Dark Orange","Heather Grey/Charcoal/Maroon","Black/White/Red","Heather Grey/Dark Green/Black","Heather Grey/Red/Black","Blue Teal/Birch/Navy","White/Aluminum/Black","White/Aluminum/Navy","White/Columbia Blue/Yellow","Mink Beige/Charcoal/Amber Gold","Navy/White/Heather Grey","Columbia Blue/White/Navy","Dark Green/White/Heather Grey"
+    ]
+  },
+  "168": {
+    label: "168 Seven Panel",
+    priceTier: "premium",
+    colors: ["Black","Loden","Navy","Black/Green Camo/Loden","Pale Khaki/Brown","Brown/Khaki","Caramel","Pale Khaki/Loden","Quarry","Charcoal","Red/Black","Charcoal/Black","Royal/Black","Charcoal/Black/White","White","Charcoal/Burnt Orange/Black","Charcoal/Old Gold","Dark Green/Black","Heather Grey/Black"]
+  },
+  "256": {
+    label: "256 Gramps",
+    priceTier: "premium",
+    colors: ["Birch/Black","Midnight Navy/White","Navy/Red","Biscuit/Black","Navy/White","Black/Black","Black/White","Pale Peach/Maroon","Red/White","Cardinal/White","Sage/White","Charcoal/White","Sand Dune/Loden","Dark Mocha/Khaki","White/Black","Dark Orange/Black","Dusty Blue/White","Dusty Red/White","Loden/Gold"]
+  },
+  "112PFP": {
+    label: "112PFP Printed Five Panel",
+    priceTier: "standard",
+    colors: ["Realtree Edge/Brown","Green Camo/Black","Digital Camo/Light Green","Green Camo/White","Desert Camo/Brown","Realtree Edge/Neon Orange","Admiral Duck Camo/Black","Realtree Edge/Neon Pink","Kryptek Highlander/Buck","Bark Duck Camo/Brown","Kryptek Inferno/Blaze Orange","Realtree Edge/Neon Yellow","Blaze Duck Camo/Blaze","Realtree Escape/Black","Kryptek Neptune/White","Kryptek Typhon/Black","Realtree Fishing Light Blue/White","Kryptek Typhon/Blaze Orange","Realtree Max-1 XT/Brown","Blizzard Duck Camo/White","Harvest Duck Camo/Light Tan","Realtree Max-7/Buck","Marsh Duck Camo/Loden","Sable Duck Camo/Black","Realtree Original/Black","Realtree Timber/Black","Saltwater Duck Camo/Charcoal","Mossy Oak Bottomland/Loden","Mossy Oak Country DNA/Black","Mossy Oak Elements Bonefish/Light Grey","Mossy Oak Habitat/Brown","Sienna Duck Camo/Loden","Kryptek Neptune/Black","Kryptek Inferno/Black","Kryptek Typhon/Neon Pink","Kryptek Typhon/Neon Yellow","Kryptek Typhon/Neon Orange","Mossy Oak Elements Blacktip/Charcoal","Realtree Fishing Light Blue/Navy"]
+  },
+  "112P": {
+    label: "112P Printed Trucker",
+    colors: ["Digital Camo/Light Green","Green Camo/Black","Green Camo/White","Kryptek Highlander/Buck","Kryptek Pontus/White","Kryptek Typhon/Black","Mossy Oak Country DNA/Black","Mossy Oak Bottomland/Loden","Realtree Edge/Brown","Realtree Max-7/Buck","Realtree Original/Black"]
+  },
+  "112PM": {
+    label: "112PM Printed Mesh",
+    colors: ["Black/Black-White Fade","Brown/Desert Camo","Green Camo/Stars & Stripes","Heather Grey/Stars & Stripes","Loden/Green Camo","Mossy Oak Country DNA/Stars & Stripes","Navy/Navy-White Fade","Silver/Grey Camo"]
+  },
+  "168P": {
+    label: "168P Printed Seven Panel",
+    colors: ["Bark Duck Camo/Brown","Harvest Duck Camo/Sandstone","Marsh Duck Camo/Loden","Saltwater Duck Camo/Ice Grey","Mossy Oak Bottomland/Loden","Realtree Advantage Classic/Khaki"]
+  },
+  "256P": {
+    label: "256P Printed Gramps",
+    colors: ["Bark Duck Camo/Brown","Blizzard Duck Camo/White","Harvest Duck Camo/Light Brown","Marsh Duck Camo/Army Olive","Saltwater Duck Camo/Charcoal","Mossy Oak Bottomland/Black","Realtree Advantage Classic/Khaki"]
+  }
+};
